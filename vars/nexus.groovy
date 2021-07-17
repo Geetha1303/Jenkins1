78a6f4715fc3340f14336e210784a088ae4e32ab
Geetha1303 <geetha@zelarsoft.com>
2021-07-17 18:09:55 +0530

    new_1

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -9,42 +9,42 @@ def nexus(COMPONENT) {
 
 def make_artifacts(APP_TYPE, COMPONENT) {
     get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}'| xargs echo -n"
-    def get_branch_exec =sh(returnStdout: true, script: get_branch)
-    def FILENAME = COMPONENT + '-' + get_branch_exec + '.zip'
-    if (COMPONENT == "frontend1") {
-        command = "zip -r ${FILENAME} ."
-        def execute_com =sh(returnStdout: true, script: command)
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+    if(COMPONENT == "frontend1") {
+        command="zip -r ${FILENAME} ."
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
-    } else if (COMPONENT == "login1") {
-        command = "zip -r ${FILENAME} main.go user.go tracing.go"
-        def execute_com =sh(returnStdout: true, script: command)
+    } else if(COMPONENT == "login1") {
+        command="zip -r ${FILENAME} main.go user.go tracing.go"
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
-    } else if (COMPONENT == "users") {
-        command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
-        def execute_com =sh(returnStdout: true, script: command)
+    } else if(COMPONENT == "users") {
+        command="cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
-    } else if (COMPONENT == "todo1") {
-        command = "zip -r ${FILENAME} ."
-        def execute_com =sh(returnStdout: true, script: command)
+    } else if(COMPONENT == "todo1") {
+        command="zip -r ${FILENAME} ."
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
     }
 }
 
 def code_build(APP_TYPE, COMPONENT) {
-    if (COMPONENT == "frontend1") {
-        command = "npm install"
-        def execute_com =sh(returnStdout: true, script: command)
+    if(COMPONENT == "frontend1") {
+        command="npm install"
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
-    } else if (COMPONENT == "users") {
-        command = "mvn clean package"
-        def execute_com =sh(returnStdout: true, script: command)
+    } else if(COMPONENT == "users") {
+        command="mvn clean package"
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
-    } else if (COMPONENT == "login1") {
-        command = " go get github.com/openzipkin/zipkin-go && go get github.com/openzipkin/zipkin-go/middleware/http &&go get github.com/openzipkin/zipkin-go/reporter/http && go get github.com/labstack/echo && go get github.com/labstack/echo/middleware &&go get github.com/labstack/gommon/log && go build main.go user.go tracing.go"
-        def execute_com =sh(returnStdout: true, script: command)
-    } else if (COMPONENT == "todo1") {
-        command = "npm install && npm i nodemon && npm link nodemon"
-        def execute_com =sh(returnStdout: true, script: command)
+    } else if(COMPONENT == "login1") {
+        command=" go get github.com/openzipkin/zipkin-go && go get github.com/openzipkin/zipkin-go/middleware/http &&go get github.com/openzipkin/zipkin-go/reporter/http && go get github.com/labstack/echo && go get github.com/labstack/echo/middleware &&go get github.com/labstack/gommon/log && go build main.go user.go tracing.go"
+        def execute_com=sh(returnStdout: true, script: command)
+    } else if(COMPONENT == "todo1") {
+        command="npm install && npm i nodemon && npm link nodemon"
+        def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
     }
 }
