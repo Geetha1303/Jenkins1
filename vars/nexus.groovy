7140380746ec02277906c8832d4bf2d667e18e7b
Geetha1303 <geetha@zelarsoft.com>
2021-07-19 15:27:24 +0530

    Latest

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -12,7 +12,7 @@ def make_artifacts(COMPONENT) {
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(COMPONENT == "frontend1") {
-        command="zip -r ${FILENAME} node_modules"
+        command="zip -r ${FILENAME} ."
         println(command)
         def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
@@ -26,7 +26,7 @@ def make_artifacts(COMPONENT) {
         def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
     } else if(COMPONENT == "todo1") {
-        command="zip -r ${FILENAME} node_modules"
+        command="zip -r ${FILENAME} ."
         println(command)
         def execute_com=sh(returnStdout: true, script: command)
         println(execute_com)
