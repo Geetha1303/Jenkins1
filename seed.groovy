c1dcb223bac476a3baba35b7b127e7f3e3f43c67
Geetha1303 <geetha@zelarsoft.com>
2021-07-28 13:01:19 +0530

    Latest

--- seed.groovy
+++ seed.groovy
@@ -40,3 +40,23 @@ for (i in 0..count) {
 
     }
 }
+pipelineJob("Deployment Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/Geetha1303/Jenkins1.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Deployment')
+            'lightweight'(true)
+        }
+    }
+}
